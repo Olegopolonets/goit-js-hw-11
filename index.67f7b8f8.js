a01911959f781959c439d3da219265173054274a
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-11-19 14:42:38 +0000

    Deploying to gh-pages from @ Olegopolonets/goit-js-hw-11@cdf2f99b6822fb0fda191b4387075585480c65a0 🚀

--- index.67f7b8f8.js
+++ index.67f7b8f8.js
@@ -1,2 +1,2 @@
 !function(){var t;function e(){"dark"==i()?document.body.classList.add("active"):document.body.classList.remove("active")}function n(){var t=arguments.length>0&&void 0!==arguments[0]?arguments[0]:"light";window.localStorage.setItem("theme",t),e()}function i(){return window.localStorage.getItem("theme")}window.document.querySelector(".switcher-input").addEventListener("change",(function(){n("dark"==i()?"light":"dark")})),n(null==(t=i())||null==t||"string"!=typeof t?"light":"dark"==t?"dark":"light")}();
-//# sourceMappingURL=index.6ffe393d.js.map
+//# sourceMappingURL=index.67f7b8f8.js.map
